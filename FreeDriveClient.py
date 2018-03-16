303cfd2d7223839398796f238212ba6e2aea7597
Rafael Teixeira <rafael@debian>
2018-03-16 15:11:01 -0300

    Changed argument structure, added the backup interval in days setting

--- FreeDriveClient.py
+++ FreeDriveClient.py
@@ -16,13 +16,6 @@ import googleapiclient
 import datetime
 import time
 
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-
 from mimetypes import MimeTypes
 
 SCOPES = 'https://www.googleapis.com/auth/drive'
@@ -49,10 +42,9 @@ class FreeDriveClient():
         if not credentials or credentials.invalid:
             flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
             flow.user_agent = APPLICATION_NAME
-            if flags:
-                credentials = tools.run_flow(flow, store, flags)
-            else: # Needed only for compatibility with Python 2.6
-                credentials = tools.run(flow, store)
+            
+            credentials = tools.run_flow(flow, store)
+            
             print('Storing credentials to ' + credential_path)
         return credentials
     
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 #coding: utf-8
 from FreeDriveClient import *
+import sys
 
-def main():    
-    path = str(input())
 
-    client = FreeDriveClient()
-    client.uploadFolder(path)
+def main():
 
-    print("Done!")
-            
+    print("Google Drive backup tool stated.")
+
+    path = str(sys.argv[1])
+    backup_interval = int(sys.argv[2])
+    last_backup = datetime.datetime.now()
+    
+    while 1:
+        current_time = datetime.datetime.now()
+        diff = current_time - last_backup
+        if(diff.days > backup_interval):
+            client = FreeDriveClient()
+            client.uploadFolder(path)
+            last_backup = datetime.datetime.now()
+            print("\nBackup successfully finished at " + last_backup.strftime('%d-%m-%Y %H:%M:%S') + "\n")
+
+
+
 if __name__ == '__main__':
     main()
