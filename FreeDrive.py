cfcd9fba4eb5c9e5f63716bbcf1886a111064201
Rafael Teixeira <rodriguesteixeirarafael@gmail.com>
2018-03-16 15:19:25 -0300

    typos

--- FreeDrive.py
+++ FreeDrive.py
@@ -4,22 +4,23 @@ import sys
 
 
 def main():
-
-    print("Google Drive backup tool stated.")
+    print("Google Drive backup tool started.\n")
 
     path = str(sys.argv[1])
     backup_interval = int(sys.argv[2])
     last_backup = datetime.datetime.now()
+    firstRun = True
     
     while 1:
         current_time = datetime.datetime.now()
         diff = current_time - last_backup
-        if(diff.days > backup_interval):
+        if diff.days >= backup_interval or firstRun:
             client = FreeDriveClient()
             client.uploadFolder(path)
             last_backup = datetime.datetime.now()
             print("\nBackup successfully finished at " + last_backup.strftime('%d-%m-%Y %H:%M:%S') + "\n")
-
+        
+        firstRun = False        
 
 
 if __name__ == '__main__':
